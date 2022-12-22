87f1449e7b87e3922aacca68443ff1ac3e79f34a
Nikita Yu Lovyagin <lovyagin@mail.com>
2022-12-22 20:06:08 +0300

    Review

--- OOP_structures/test2_queue.cpp
+++ OOP_structures/test2_queue.cpp
@@ -11,7 +11,7 @@ public:
     numerate(T start = 0, T _shift = 1): seed(start), shift(_shift) {};
     T operator () () {T old = seed; seed += shift; return old; }
 private:
-    T seed, shift;
+    T seed, shift;    
 };
 
 template <class T, class C> T sum (const C& c)
@@ -30,19 +30,6 @@ int main()
     s1.push(1);
     s1.push(2);
     s1.push(3);
-<<<<<<< HEAD
-
-    s2.push(5);
-    s2 = s1;
-    s2.push(4);
-
-    cout << s1 << endl;          // 1->2->3
-    cout << s1.size() << endl;
-
-    cout << s2 << endl;          // 1->2->3->4
-    cout << s2.size() << endl;
-
-=======
     
     s2.push(5);
     s2 = s1;
@@ -54,33 +41,20 @@ int main()
     cout << s2 << endl;          // 1->2->3->4
     cout << s2.size() << endl;
     
->>>>>>> 401fd6e3a9980ae3d9eb1c814320c80d1eef8805
     swap(s1, s2);
-
+    
     cout << s1 << endl;          // 1->2->3->4
     cout << s1.size() << endl;
-<<<<<<< HEAD
-
-    cout << s2 << endl;          // 1->2->3
-    cout << s2.size() << endl;
-
-=======
     
     cout << s2 << endl;          // 1->2->3
     cout << s2.size() << endl;
     
->>>>>>> 401fd6e3a9980ae3d9eb1c814320c80d1eef8805
     generate(s1.begin(), s1.end(), f);
 
     cout << s1 << endl;          // 100->101->102->103
     cout << s1.size() << endl;
-<<<<<<< HEAD
-
-    //cout << sum<int,List>(s2) << endl;     // 6
-=======
     
     cout << sum<int,List<int>>(s2) << endl;     // 6
->>>>>>> 401fd6e3a9980ae3d9eb1c814320c80d1eef8805
 
     return 0;
 }
--- OOP_structures/test2_stack.cpp
+++ OOP_structures/test2_stack.cpp
@@ -11,7 +11,7 @@ public:
     numerate(T start = 0, T _shift = 1): seed(start), shift(_shift) {};
     T operator () () {T old = seed; seed += shift; return old; }
 private:
-    T seed, shift;
+    T seed, shift;    
 };
 
 template <class T, class C> T sum (const C& c)
@@ -25,28 +25,11 @@ template <class T, class C> T sum (const C& c)
 int main()
 {
     Stack<int> s1, s2;
-<<<<<<< HEAD
-    numerate<int> f(100);
-=======
     numerate f(100);
->>>>>>> 401fd6e3a9980ae3d9eb1c814320c80d1eef8805
 
     s1.push(1);
     s1.push(2);
     s1.push(3);
-<<<<<<< HEAD
-
-    s2.push(5);
-    s2 = s1;
-    s2.push(4);
-
-    cout << s1 << endl;          // 3->2->1
-    cout << s1.size() << endl;
-
-    cout << s2 << endl;          // 4->3->2->1
-    cout << s2.size() << endl;
-
-=======
     
     s2.push(5);
     s2 = s1;
@@ -58,33 +41,20 @@ int main()
     cout << s2 << endl;          // 4->3->2->1
     cout << s2.size() << endl;
     
->>>>>>> 401fd6e3a9980ae3d9eb1c814320c80d1eef8805
     swap(s1, s2);
-
+    
     cout << s1 << endl;          // 4->3->2->1
     cout << s1.size() << endl;
-<<<<<<< HEAD
-
-    cout << s2 << endl;          // 3->2->1
-    cout << s2.size() << endl;
-
-=======
     
     cout << s2 << endl;          // 3->2->1
     cout << s2.size() << endl;
     
->>>>>>> 401fd6e3a9980ae3d9eb1c814320c80d1eef8805
     generate(s1.begin(), s1.end(), f);
 
     cout << s1 << endl;          // 100->101->102->103
     cout << s1.size() << endl;
-<<<<<<< HEAD
-
-    //cout << sum<int,List>(s2) << endl;     // 6
-=======
     
     cout << sum<int,List<int>>(s2) << endl;     // 6
->>>>>>> 401fd6e3a9980ae3d9eb1c814320c80d1eef8805
 
     return 0;
 }
